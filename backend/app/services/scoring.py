6572acb3e46afbe3dad29d257774ac798d5a519b
Jordan Junaidi <jordanrjunaidi@gmail.com>
2025-11-15 15:09:16 -0500

    Added co2 scores

--- backend/app/services/scoring.py
+++ backend/app/services/scoring.py
@@ -1,5 +1,7 @@
-from app.services.data_loader import supplier_data
+from app.services.data_loader import supplier_data, supplier_metadata
 from typing import List, Dict
+
+SUPPLIERS = ["CLF", "CMC", "FRD", "MT", "STLD", "X"]
 
 # Min gets set to 0, Max gets set to 1
 def normalizer(values: List[float]) -> List[float]:
@@ -23,26 +25,35 @@ def inverse_normalizer(values: List[float]) -> List[float]:
     
 # Calculates score based on cost
 def calculate_cost_scores() -> Dict[str, float]:
-    companies = list(supplier_data.keys())
-    closing_prices = [supplier_data[company]["avg_price"] for company in companies]
+    closing_prices = [supplier_data[company]["avg_price"] for company in SUPPLIERS]
 
     # Want lowest price to have highest score
     cost_scores = inverse_normalizer(closing_prices)
 
     results = {}
-    for company, score in zip(companies, cost_scores):
+    for company, score in zip(SUPPLIERS, cost_scores):
         results[company] = score
     
     return results
 
 def calculate_risk_scores() -> Dict[str, float]:
-    companies = list(supplier_data.keys())
-    volatilities = [supplier_data[company]["volatility"] for company in companies]
+    volatilities = [supplier_data[company]["volatility"] for company in SUPPLIERS]
 
     risk_scores = inverse_normalizer(volatilities)
 
     results = {}
-    for company, score in zip(companies, risk_scores):
+    for company, score in zip(SUPPLIERS, risk_scores):
+        results[company] = score
+
+    return results
+
+def calculate_co2_scores() -> Dict[str, float]:
+    co2_usages = [supplier_metadata[company]["co2_per_ton"] for company in SUPPLIERS]
+
+    co2_scores = inverse_normalizer(co2_usages)
+
+    results = {}
+    for company, score in zip(SUPPLIERS, co2_scores):
         results[company] = score
 
     return results
