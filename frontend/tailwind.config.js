e5de2326cb6d72df8e397cf349e952f43e56475f
migingyn <nmigi32@gmail.com>
2025-11-15 14:30:07 -0800

    implemented navbar, landing page

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        notoSans: ['"Noto Sans"', 'sans-serif'],
+        robotoSerif: ['"Roboto Serif"', 'serif'],
+      },
+      colors: {
+        ecoGreen: '#065c30',
+        figGray: '#6d89b3',
+        figBlue: '#122036',
+      },
+    },
   },
   plugins: [],
 };
